c72eb95cf2c6648279a62cf31536c51a10eb030c
qhj <me@qhj.moe>
2021-03-19 10:41:30 +0800

    init & add modern-normalize

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <HelloWorld msg="网易云音乐 听见好时光" />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  
+  export default defineComponent({
+    name: 'App',
+    components: {
+      HelloWorld
+    }
+  })
+</script>
+
+<style>
+  
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'HelloWorld',
+    props: {
+      msg: {
+        type: String,
+        required: true
+      }
+    }
+  })
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+</style>
