860ac4ea7f73901ac69b81fea478f1ed413d488a
qhj <me@qhj.moe>
2021-03-22 18:35:30 +0800

    use element-plus & vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'modern-normalize/modern-normalize.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
-createApp(App).mount('#app')
+import { router } from './router'
+
+createApp(App)
+  .use(ElementPlus)
+  .use(router)
+  .mount('#app')
